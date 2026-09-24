6dbfe6239da4e783f98e9dd4c02c02639053f3a4
Pasang Lama <snowpasang@gmail.com>
2026-09-24 15:07:30 +0800

    two_sum solution

---
+++ two_sum.py
@@ -0,0 +1,12 @@
+#input: nums = [2,7,11,15] target = 9
+#output : [0,1]
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+
+                if nums[i] + nums[j] == target:
+                    return [i, j]
